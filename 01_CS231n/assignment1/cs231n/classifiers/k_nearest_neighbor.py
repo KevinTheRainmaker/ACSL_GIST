464f0f2f3acc84a312e5072ce121d1890f5b8ec2
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-09-20 16:29:56 +0900

    feat(assignment1): knn

--- 01_CS231n/assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ 01_CS231n/assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -102,7 +102,7 @@ class KNearestNeighbor(object):
             #######################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            dists[i] = np.sqrt(np.sum((self.X_train - X[i])**2, axis=1))
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
@@ -132,7 +132,9 @@ class KNearestNeighbor(object):
         #########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        dists = np.sqrt((-2 * np.dot(X, self.X_train.T))
+                        + np.sum(X**2, axis=1, keepdims=True)
+                        + np.sum(self.X_train**2, axis=1))
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return dists
